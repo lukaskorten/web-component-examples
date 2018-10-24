e54058f7d6e47dfede35ffc0266256979f391bf5
Lukas Korten <lukas.korten@adesso.de>
2018-10-24 20:54:13 +0200

    add fontawesome icons; implement icon-button

--- icon-button/main.js
+++ icon-button/main.js
@@ -1,38 +1,20 @@
 class IconButton extends HTMLButtonElement {
     constructor() {
-      super();      
-
-      const wrapper = document.createElement('span');
-      wrapper.setAttribute('class', 'wrapper');
-
+      super();
+      
       const iconType = this.getAttribute('icon-type');
       const icon = this.getAttribute('icon');
 
       const popup = document.createElement('span');
-      wrapper.setAttribute('class', 'pop-up');
+      popup.setAttribute('class', 'pop-up');
       popup.innerHTML = this.innerHTML;
-      this.innerHTML = `<i class="${iconType} fa-${icon}"></i>`;
+      this.innerHTML = '';
 
-      const style = document.createElement('style');
-      style.textContent = `
-        
-        .wrapper {
-            position: relative;            
-        }
-
-        .pop-up {
-            position: absolute;
-            padding: 15px;
-            border: 1px solid #ddd;
-
-        }
-      `;
-
-      document.head.appendChild(style);  
-      wrapper.appendChild(popup);
-      this.appendChild(wrapper);
+      const iconTag = document.createElement('i');
+      iconTag.setAttribute('class', `fa${iconType} fa-${icon}`);
       
-
+      this.appendChild(iconTag);
+      this.appendChild(popup);
 
     }
 
@@ -56,17 +38,6 @@ class IconButton extends HTMLButtonElement {
         return ['icon', 'icon-type'];
     }
     
-    attributeChangedCallback(name, oldValue, newValue) {
-        switch (name) {
-            case 'icon':
-            console.log(`Icon changed from ${oldValue} to ${newValue}`);
-            break;
-            case 'icon-type':
-            console.log(`Icon-Type changed from ${oldValue} to ${newValue}`);
-            break;
-        }
-    }
-  
     
   }
   
